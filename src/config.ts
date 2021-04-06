9794fee1d1ab200e1dc91ebfb791943573cb7b78
kritzona <virell.m@yandex.ru>
2021-04-06 16:15:38 +0300

    Адрес сервера для API меняется автоматически в зависимости от окружения

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,14 @@
+const host = {
+  development: 'http://localhost:3000',
+  production: 'https://chuckchuck-server.herokuapp.com',
+}
+
 const config = {
   backend: {
-    host: 'https://chuckchuck-server.herokuapp.com',
+    host:
+      process.env.NODE_ENV === 'development'
+        ? host.development
+        : host.production,
     port: null,
     apiFolder: '/api',
     servicesFolder: '/services',
